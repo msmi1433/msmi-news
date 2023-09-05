9c7919e066d340cd7b9380fd5a41b9a89ac9b7c9
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-09-05 14:54:14 +0100

    completes single article rendering

--- src/components/pages/SingleArticle.jsx
+++ src/components/pages/SingleArticle.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { getArticleAuthor, getSingleArticle } from "../apiCalls";
+import { Link } from "react-router-dom";
 
 const SingleArticle = () => {
   const { article_id } = useParams();
@@ -29,7 +30,47 @@ const SingleArticle = () => {
       });
   }, []);
 
-  return <div>hello</div>;
+  if (isLoading) return <h3 className="loading-message">Loading...</h3>;
+  if (isError)
+    return (
+      <h3 className="error-message">Something went wrong - please try again</h3>
+    );
+
+  return (
+    <section className="single-article container">
+      <div className="single-article head">
+        <div className="single-article title">
+          <h1 className="single-article h1">{article.title}</h1>
+          <p className="single-article topic">{article.topic}</p>
+          <img
+            src={article.article_img_url}
+            alt={`${article.title} image`}
+            className="single-article image"
+          />
+        </div>
+        <div className="single-article info-box">
+          <Link>
+            <p className="single-article author">{author.name}</p>
+          </Link>
+          <p className="single-article date">
+            Published:{" "}
+            {new Date(article.created_at).toLocaleDateString({
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
+          <p className="single-article votes">Article votes: {article.votes}</p>
+          <button className="upvote-button">Upvote 👍</button>
+          <button className="downvote-button">Downvote 👎</button>
+        </div>
+      </div>
+      <div className="single-article body">
+        <p>{article.body}</p>
+      </div>
+    </section>
+  );
 };
 
 export default SingleArticle;
