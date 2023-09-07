fd295936f7afa25e8021103b3da2bafa64d7f62e
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-09-07 10:30:55 +0100

    conditional rendering of sort buttons on homepage

--- src/components/features/ArticleContainer.jsx
+++ src/components/features/ArticleContainer.jsx
@@ -8,6 +8,7 @@ const ArticleContainer = ({ pageNumber, category }) => {
   const [isError, setIsError] = useState(false);
   const [sortBy, setSortBy] = useState("created_at");
   const [orderBy, setOrderBy] = useState("DESC");
+  const [pathname, setPathname] = useState(window.location.pathname);
 
   useEffect(() => {
     setIsLoading(true);
@@ -30,27 +31,31 @@ const ArticleContainer = ({ pageNumber, category }) => {
 
   return (
     <section className="article-container">
-      <select
-        defaultValue={sortBy}
-        className="sort-by selector"
-        onChange={(e) => {
-          setSortBy(e.target.value);
-        }}
-      >
-        <option value="created_at">Date published</option>
-        <option value="comment_count">Comment count</option>
-        <option value="votes">Votes</option>
-      </select>
-      <select
-        defaultValue={orderBy}
-        className="order-by selector"
-        onChange={(e) => {
-          setOrderBy(e.target.value);
-        }}
-      >
-        <option value="DESC">Descending</option>
-        <option value="ASC">Ascending</option>
-      </select>
+      {pathname !== "/" ? (
+        <>
+          <select
+            defaultValue={sortBy}
+            className="sort-by selector"
+            onChange={(e) => {
+              setSortBy(e.target.value);
+            }}
+          >
+            <option value="created_at">Date published</option>
+            <option value="comment_count">Comment count</option>
+            <option value="votes">Votes</option>
+          </select>
+          <select
+            defaultValue={orderBy}
+            className="order-by selector"
+            onChange={(e) => {
+              setOrderBy(e.target.value);
+            }}
+          >
+            <option value="DESC">Descending</option>
+            <option value="ASC">Ascending</option>
+          </select>
+        </>
+      ) : null}
       <ol className="articles-ol">
         {articleList.map((article) => {
           return (
